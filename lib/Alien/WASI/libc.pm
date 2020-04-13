742e2644bb6815e49dec3ff9d81efdf5e1a08636
Graham Ollis <plicease@cpan.org>
2020-04-13 08:23:16 -0600

    Initial structure

---
+++ lib/Alien/WASI/libc.pm
@@ -0,0 +1,12 @@
+package Alien::WASI::libc;
+
+use strict;
+use warnings;
+use 5.008001;
+
+# ABSTRACT: Alien to find or get libc for WASI 
+# VERSION
+
+1;
+
+
---
+++ t/alien_wasi_libc.t
@@ -0,0 +1,8 @@
+use Test2::V0 -no_srand => 1;
+use Alien::WASI::libc;
+
+ok 1, 'todo';
+
+done_testing;
+
+
